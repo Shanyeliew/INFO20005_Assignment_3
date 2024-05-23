0703b1da09306beb512d8f2d44138f5ec4fddd71
Shanyeliew <shanye327@gmail.com>
2024-05-24 03:09:11 +1000

    cart overlay and tidy up the layout

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
-//Handle the added-to-cart popup
-
 var addButton = document.querySelector('.product-button');
 var addButtonCard = document.getElementsByClassName('product-card-button');
 var popUp = document.querySelector('.added-cart-popup');
 var continueShopping = document.querySelector('.continue-shopping');
 
-addButton.addEventListener('click', function() {
-    popUp.classList.add('show');
+var cartOverlay = document.getElementById('cart-overlay');
+var cart = document.getElementById('cart-container');
+var cartButton = document.querySelector('#cart');
+var closeCartButton = document.getElementById('close');
+
+//Handle to cart overlay
+
+cartButton.addEventListener('click', function() {
+    
+    cartOverlay.style.right = '0';
+    cart.style.right = '0';
+    console.log('clicked');
 });
 
-for(var i = 0; i < addButtonCard.length; i++) {
-    addButtonCard[i].addEventListener('click', function() {
+closeCartButton.addEventListener('click', function() {
+    cart.style.right = '-100%';
+    cartOverlay.style.right = '-100%';
+    console.log('clicked');
+});
+
+function openCart() {
+    cart.style.right = '0';
+    cartOverlay.style.right = '0';
+}
+
+//Handle the added-to-cart popup
+if(window.location.href.indexOf('product_1.html') > -1 || window.location.href.indexOf('product_2.html') > -1) {
+    addButton.addEventListener('click', function() {
         popUp.classList.add('show');
+        console.log('clicked');
     });
+    
+    continueShopping.addEventListener('click', function() {
+        popUp.classList.remove('show');
+    });
+    
+}
+
+if(window.location.href.indexOf('product_list.html') > -1) {
+    for (var i = 0; i < addButtonCard.length; i++) {
+        addButtonCard[i].addEventListener('click', function() {
+            popUp.classList.add('show');
+            console.log('clicked');
+        });
+    }
+
+    for (var i = 0; i < addButtonCard.length; i++) {
+        continueShopping.addEventListener('click', function() {
+            popUp.classList.remove('show');
+        });
+    }
 }
 
 
-continueShopping.addEventListener('click', function() {
-    popUp.classList.remove('show');
-});
 
-Window.addEventListener('click', function(event) {
-    if(event.target == popUp) {
-        popUp.classList.remove('show');
-    }
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var buttons = document.querySelectorAll('.product-card-button');
-
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            popUp.classList.add('show');
-        });
-    });
-});
